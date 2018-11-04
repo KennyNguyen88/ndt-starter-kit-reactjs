b6029a10e08a7222a48d4c8009b610073f9995d5
KennyNguyen88 <giatrangrua@gmail.com>
2018-11-04 10:27:28 +0700

    [Fontawesome]

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(faLaptopCode);
+
 export const Landing = () => {
     return (
-        <div>This is Landing</div>
+        <div>
+            This is Landing
+            <FontAwesomeIcon icon="laptop-code" size="2x"/>
+        </div>
     )
 };
 
