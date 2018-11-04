3de8d98a3c29c4a0247c2b6cc57d1618a8335c03
KennyNguyen88 <giatrangrua@gmail.com>
2018-11-04 10:38:06 +0700

    [Fontawesome] - 2 more icons set: brands, regular - can use without syntax library

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
 import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-library.add(faLaptopCode);
 
 export const Landing = () => {
     return (
         <div>
             This is Landing
-            <FontAwesomeIcon icon="laptop-code" size="2x"/>
+            <FontAwesomeIcon icon={faLaptopCode} size="2x"/>
         </div>
     )
 };
